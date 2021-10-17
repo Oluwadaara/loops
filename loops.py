8ee6929ad54a94f6fa246b30354618f2e761f15f
Oluwadaara <89576468+Oluwadaara@users.noreply.github.com>
2021-10-17 18:58:02 +0100

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#WHILE LOOP
+# Take user input
+number = 2  
+
+# Condition of the while loop
+while number < 5 :  
+    print("Hey you!")
+    # Increment the value of the variable "number by 1"
+    number = number+1
+
+
+# In[2]:
+
+
+#FOR LOOP
+# Print "How you doing?" 5 times
+for number in range(5):
+    print("How you doing?")
+
+
+# In[ ]:
+
+
+
+
